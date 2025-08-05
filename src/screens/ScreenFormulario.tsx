c2f406d4af4fe4892fe496f3db1056342eea0989
GersonTeranGT <guapygt543@gmail.com>
2025-08-05 18:24:27 -0500

    mejoras

--- src/screens/ScreenFormulario.tsx
+++ src/screens/ScreenFormulario.tsx
@@ -3,26 +3,29 @@ import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 
 export const ScreenFormulario = () => {
+    //Validacion para que los inputs acepten solo numeros
     const validacion = /^-?\d*\.?\d*$/;
     const [valor1, setValor1] = useState('');
     const [valor2, setValor2] = useState('');
-    const [resultado, setResultado] = useState<string | null>(null);
+    const [resultado, setResultado] = useState<number | string>('');
 
     const dividir = () => {
         const numeroValor1 = Number(valor1);
         const numeroValor2 = Number(valor2);
-        if (!valor1 || !valor2) {
+        let division: number = 0;
+        if (!valor1 || !valor2) { //si el input esta vacio el valor es falso y >!< lo convierte en verdadero 
+            //y la condicion se cumple si no es falso
             setResultado('');
             Alert.alert('Sin datos para dividir. Por favor, ingrese ambos números');
         } else if (numeroValor1 === 0 && numeroValor2 === 0) {
             setResultado('Indeterminacion');
             Alert.alert('Indeterminacion');
         } else if (numeroValor2 === 0) {
-            setResultado('No existe division para cero');
-            Alert.alert('No existe division para cero');
+            setResultado('No existe división para cero');
+            Alert.alert('No existe división para cero');
         } else {
-            const division = (numeroValor1 / numeroValor2).toFixed(2);
-            setResultado(division);
+            division = (numeroValor1 / numeroValor2);
+            setResultado(Number(division).toFixed(2));
         }
     }
 
@@ -34,6 +37,8 @@ export const ScreenFormulario = () => {
                 style={styles.input}
                 maxLength={10}
                 onChangeText={text => {
+                    //comprobando la validación de los inputs
+                    //si cumple con la validacion se guarda en la variable si no no
                     if (validacion.test(text)) {
                         setValor1(text);
                     }
